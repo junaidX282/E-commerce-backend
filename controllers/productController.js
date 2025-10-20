a5967651ccb8e28dfc8a95d1b5815d0fdc10cabc
junaid <junaid.vjunu9895@gmail.com>
2025-10-20 18:00:09 +0530

    Added admin authentication feature

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,6 +11,7 @@ const addProduct = async (req, res) =>{
         const image3 = req.files.image3 && req.files.image3[0]
         const image4 = req.files.image4 && req.files.image4[0]
 
+        
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
 
         let imagesUrl = await Promise.all(
@@ -52,18 +53,45 @@ const addProduct = async (req, res) =>{
 // function for list product
 const listProducts = async (req, res) =>{
 
+    try {
+
+        const products = await productModel.find({})
+        res.json({success:true, products})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
 }
 
 
 // function for removing product
 const removeProduct = async (req, res) =>{
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "product removed"})
 
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message:error.message})
+    }
 }
 
 
 // function for single product info
 const singleProduct = async (req, res) =>{
+    try {
+        
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
 
+        res.json({success: true, product})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+        
+    } 
 }
 
 export {addProduct, listProducts, removeProduct, singleProduct}
